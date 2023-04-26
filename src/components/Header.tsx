ecc0248aab6e0eb20dc75594017272fb7872d3d1
ragehuma <116360742+ragehuma@users.noreply.github.com>
2023-04-26 21:01:46 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
       <div className="flex flex-row py-2 justify-between items-center w-full">
         <div className="text-2xl font-bold text-left bg-gray-100 text-gray-800">
           <span className="font-bold ml-2 decoration-purple-500 animate-text text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-            SpeechGPT
+            AIpolice
           </span>
         </div>
         <div>
